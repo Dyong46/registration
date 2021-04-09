ec3cedcc7143a77efcea5b2da8ddf1d84cd820ee
Maxime Asselin <xorbis@gmail.com>
2021-04-09 13:13:42 -0400

    Base setup

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+		jest: true
+	},
+	extends: ["eslint:recommended"],
+	ignorePatterns: ["node_modules"],
+	rules: {
+		"object-curly-newline": ["error", "never"]
+	},
+	overrides: [
+		{
+			files: ["**/*.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "tsconfig.json",
+				sourceType: "module"
+			},
+			plugins: ["@typescript-eslint/eslint-plugin"],
+			extends: [
+				"plugin:@typescript-eslint/recommended",
+				"plugin:prettier/recommended"
+			],
+			rules: {
+				"@typescript-eslint/interface-name-prefix": "off",
+				"@typescript-eslint/explicit-function-return-type": "off",
+				"@typescript-eslint/explicit-module-boundary-types": "off",
+				"@typescript-eslint/no-explicit-any": "off"
+			}
+		},
+		{
+			files: ["**/*.js"],
+			extends: ["eslint:recommended", "plugin:prettier/recommended"]
+		}
+	]
+};
