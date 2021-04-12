e518cb0080698313facf568828812fdebbdb0c9b
Maxime Asselin <xorbis@gmail.com>
2021-04-12 08:17:15 -0400

    Swagger, Throttle, main setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			cache: true,
+			envFilePath: [
+				"env/development.local.env",
+				"env/development.env",
+				"env/production.env"
+			]
+		}),
+		ThrottlerModule.forRoot({
+			ttl: 10,
+			limit: 25,
+			ignoreUserAgents: [/rely-/gi]
+		})
+	],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard
+		},
+		AppService
+	]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,71 @@
+import { join } from "path";
+import * as express from "express";
+import * as helmet from "helmet";
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import * as Package from "../package.json";
+import { swaggerOptions } from "./swagger";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		cors: true
+	});
+
+	// Augment express limits
+	app.use(
+		express.json({
+			limit: "8mb"
+		})
+	);
+
+	app.use(
+		express.urlencoded({
+			limit: "8mb",
+			extended: true
+		})
+	);
+
+	// Setup Helmet
+	app.use(helmet());
+
+	// Set global prefix to /api when in production
+	app.setGlobalPrefix(process.env.SERVICE);
+
+	// Server static assets
+	app.useStaticAssets(join(__dirname, "../..", "assets"));
+
+	// Pipes
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
+	);
+
+	// Swagger setup
+	const config = new DocumentBuilder()
+		.setTitle(`${process.env.SERVICE} service`)
+		.setDescription("Rely micro services boilerplate")
+		.setVersion(Package.version)
+		.setBasePath(process.env.SERVICE)
+		.addBearerAuth({
+			in: "header",
+			type: "http"
+		})
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup(
+		`${process.env.SERVICE}/docs`,
+		app,
+		document,
+		swaggerOptions
+	);
+
+	await app.listen(process.env.PORT);
 }
 bootstrap();
