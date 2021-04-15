eba35a9f9ad970a131ef0312e6ef2267f6d25c5e
Maxime Asselin <xorbis@gmail.com>
2021-04-15 13:36:11 -0400

    Fixing stuff.

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,19 @@
-import * as express from "express";
-import * as helmet from "helmet";
-import { ValidationPipe } from "@nestjs/common";
+import { GooglePubSubOptions } from "@algoan/pubsub";
+import { INestMicroservice } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { GCPubSubServer } from "@algoan/nestjs-google-pubsub-microservice";
 import { AppModule } from "./app.module";
-import * as Package from "../package.json";
-import { swaggerOptions } from "./swagger";
-
-function setupSwagger(app: NestExpressApplication) {
-	const config = new DocumentBuilder()
-		.setTitle(`${process.env.SERVICE} service`)
-		.setDescription("Rely micro services boilerplate")
-		.setVersion(Package.version)
-		.addBearerAuth({
-			in: "header",
-			type: "http"
-		})
-		.build();
-
-	const document = SwaggerModule.createDocument(app, config);
-
-	SwaggerModule.setup(
-		`${process.env.SERVICE}/docs`,
-		app,
-		document,
-		swaggerOptions
-	);
-}
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-		cors: true
+	const options: GooglePubSubOptions = {
+		projectId: "test",
+		subscriptionsPrefix: "test-app"
+	};
+
+	const app: INestMicroservice = await NestFactory.create(AppModule, {
+		strategy: new GCPubSubServer(options)
 	});
 
-	app.use(
-		express.json({
-			limit: "8mb"
-		})
-	);
-
-	app.use(
-		express.urlencoded({
-			limit: "8mb",
-			extended: true
-		})
-	);
-
-	app.use(helmet());
-	app.setGlobalPrefix(process.env.SERVICE);
-
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true,
-			whitelist: true,
-			forbidNonWhitelisted: true
-		})
-	);
-
-	setupSwagger(app);
-	await app.listen(process.env.PORT);
+	await app.listen(() => console.log("Server running!"));
 }
-
 bootstrap();
--- src/middlewares/logger.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
-
-@Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-	use(req, res, next: () => void) {
-		Logger.log(`${req.method} ${req.baseUrl}`, "LoggerMiddleware");
-		next();
-	}
-}
