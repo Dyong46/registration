6a8c14caedd2c158651cfe273da74a2eec299487
Maxime Asselin <xorbis@gmail.com>
2021-04-15 16:25:54 -0400

    Test lint-staged

--- src/pubsub/pubsub.module.ts
+++ src/pubsub/pubsub.module.ts
@@ -2,6 +2,6 @@ import { Module } from "@nestjs/common";
 import { PubSubService } from "./pubsub.service";
 
 @Module({
-	providers: [PubSubService]
+			providers: [PubSubService]
 })
 export class PubSubModule {}
