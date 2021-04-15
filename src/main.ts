26cd0ed59acff5f27f441318273e09bb7691faa0
Maxime Asselin <xorbis@gmail.com>
2021-04-15 16:59:21 -0400

    Removed husky and lint-staged, not working

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { GooglePubSubOptions } from "@algoan/pubsub";
 async function bootstrap() {
 	const pubSubOptions: GooglePubSubOptions = {
 		projectId: process.env.PROJECT,
-				subscriptionsPrefix: process.env.SERVICE,
+		subscriptionsPrefix: process.env.SERVICE,
 		topicsPrefix: process.env.SERVICE,
 		topicsSeparator: "."
 	};
