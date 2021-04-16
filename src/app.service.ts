0b57355afd414046fa831c60e3e0f9c2943df964
Maxime Asselin <xorbis@gmail.com>
2021-04-16 09:37:25 -0400

    Pub/Sub is uni directional and at least once, won't work for a request-response pattern.

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,17 +6,27 @@ import { EventData } from "./types";
 @Injectable()
 export class AppService {
 	constructor(private readonly pubSub: PubSubService) {
-		this.sendtest();
+		this.sendTest();
 	}
 
-	async sendtest() {
+	async sendTest() {
 		await this.pubSub.emit("boilerplate.test", {
 			foo: "bar"
+		}, {
+			metadata: {
+				reply: `${process.env.SERVICE}.test.reply`
+			}
 		});
 	}
 
 	handleTestEvent(data: EmittedMessage<EventData>) {
 		Logger.debug(data.payload, "AppService::handleTestEvent");
+		Logger.debug(data.metadata, "AppService::handleTestEvent");
+		return data.payload;
+	}
+
+	handleTestReplyEvent(data: EmittedMessage<EventData>) {
+		Logger.debug(data.payload, "AppService::handleTestReplyEvent");
 		return data.payload;
 	}
 }
