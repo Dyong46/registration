c0fc2a28488f5f9b7b368fb4fd460a6c3a2babd1
Maxime Asselin <xorbis@gmail.com>
2021-04-20 14:50:03 -0400

    Added Message test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,26 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Logger } from "@nestjs/common";
 import { MessagePattern } from "@nestjs/microservices";
 import { ClientService } from "./client/client.service";
 import { DataObject } from "./types";
 
 @Controller()
 export class AppController {
-	constructor(private readonly clientService: ClientService) {}
+	constructor(private readonly clientService: ClientService) {
+		Logger.debug("Starting test", "AppController::constructor");
+		setTimeout(async () => {
+			Logger.debug("Sending test", "AppController::constructor");
+
+			await this.clientService.send("boilerplate.test", {
+				message: `${process.env.SERVICE} test message`
+			});
+
+			Logger.debug("Test completed", "AppController::constructor");
+		}, 2000);
+	}
 
 	@MessagePattern("boilerplate.test")
 	test(data: DataObject): DataObject {
+		Logger.debug(data, "AppController::test");
 		return data;
 	}
 }
