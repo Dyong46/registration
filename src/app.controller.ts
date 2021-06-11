20293e3e9488fb6fe7be5d6126dba7cc896398df
Maxime Asselin <masselin@ploggmedia.com>
2021-06-11 12:30:26 -0400

    Root is public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
+import { Public } from "@plogg-rely/microservices";
 import { AppService } from "./app.service";
 
 @Controller("")
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
+	@Public()
 	@Get("")
 	get() {
 		return this.appService.get();
