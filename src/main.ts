f206968b75348375dc606abe73d6d9b9bbf06d76
Maxime Asselin <masselin@ploggmedia.com>
2021-06-23 14:11:25 -0400

    lint

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import * as DebugAgent from "@google-cloud/debug-agent";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
-import { MessageInterceptor, ReceiveMessagePipe } from "@plogg-rely/microservices";
+import {
+	MessageInterceptor,
+	ReceiveMessagePipe
+} from "@plogg-rely/microservices";
 
 import { AppModule } from "./app.module";
 
