d6c3ef5b951fa058516579a8f19958facd15c71a
Maxime Asselin <masselin@ploggmedia.com>
2021-08-03 17:54:59 -0400

    Fix timezone problem

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -14,6 +14,7 @@ export default {
 	username: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
+	timezone: "UTC",
 	entities: ["./dist/**/*.entity.js"],
 	logging: !production,
 	maxQueryExecutionTime: 5000,
