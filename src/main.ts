810a5b3e97cf10021b51c9b98b4ae8a5fffb0705
Maxime Asselin <masselin@ploggmedia.com>
2023-01-24 16:49:57 -0500

    Fix bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,10 @@
-import { Logger, ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import * as apm from "elastic-apm-node";
+import * as Package from "../package.json";
 
 import { AppModule } from "./app.module";
-import { ReceiveMessagePipe } from "./microservices/message.pipe";
+import { bootstrap } from "./microservices/app";
 
-apm.start({
-	active: process.env.NODE_ENV === "production"
+bootstrap(AppModule, {
+	title: "Boilerplate Service",
+	description: "Boilerplate service API used as a template on Github.",
+	version: Package.version
 });
-
-const logger = new Logger("main");
-
-async function bootstrap() {
-	if (AppModule.production()) {
-		process.env.NO_COLOR = "true";
-	}
-
-	const app = await NestFactory.create(AppModule);
-
-	// Service URL prefix
-	app.setGlobalPrefix(process.env.SERVICE);
-
-	// Pipes
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true,
-			forbidNonWhitelisted: true,
-			forbidUnknownValues: true
-		}),
-		new ReceiveMessagePipe()
-	);
-
-	// Listen
-	await app.listen(process.env.PORT, "0.0.0.0");
-	logger.log(`Service ${process.env.SERVICE} on ${await app.getUrl()}`);
-}
-
-bootstrap();
